59ce63c3aac24601633b2f55cb8a7785213f9df3
janus1947 <devuser1947@gmail.com>
2024-08-21 09:36:28 -0400

    push refactor

--- src/components/InputItemsBur.vue
+++ src/components/InputItemsBur.vue
@@ -3,18 +3,18 @@ import useburAxios from '@/composables/use-burAxios';
 import { useBurStore } from '@/stores/burStore';
 import { useRoofListStore } from '@/stores/roofList';
 import { storeToRefs } from 'pinia';
-import { onMounted, onUpdated, reactive, ref, watch, watchEffect } from 'vue';
+import { onMounted, reactive, ref, watch, watchEffect } from 'vue';
 import DripEdgeComponent from './DripEdgeComponent.vue';
 
-const { materials, tMaters, bMaters, systemHW, systemHM, systemSA } = useburAxios();
+const { bMaters, systemHW, systemHM, systemSA } = useburAxios();
 const storebur = useBurStore();
 const storeroof = useRoofListStore();
 const { roofList } = storeToRefs(storeroof);
 const { burinput } = storeToRefs(storebur);
 let isDialog = ref(false);
 const roofType = ref('lowslope');
-console.log;
-onUpdated(() => {
+
+const agreement = onMounted(() => {
     // if (roofType.value === 'lowslope') {
     isDialog = true;
     // }
@@ -35,6 +35,7 @@ const dims = reactive({
     height: '',
     perimeter: ''
 });
+const type = ref([{ name: '--Select Deck Type--' }, { name: '- 5/8" Plywood -' }, { name: '- 3/4" Plywood -' }, { name: '- 1" x 6" T & G -' }, { name: '- 1" x 8" T & G -' }, { name: '- Existing 1/2" Plywood -' }]);
 
 const selectedBur = ref();
 const mat = ref();
@@ -103,29 +104,29 @@ function updateselectSystem() {
 }
 </script>
 <template>
-    <div class="card flex flex-col md:flex-row gap-4 mt-10 bg-white shadow-lg shadow-cyan-800" style="margin-left: 50px">
+    <div class="flex flex-col w-1/2 gap-4 bg-white shadow-lg shadow-cyan-800" style="margin-left: 50px">
         <agreements-dialog-lowslope v-if="isDialog !== true"></agreements-dialog-lowslope>
 
         <div class="card flex flex-col gap-6">
-            <div class="w-64 gap-4" style="margin-left: 12px">
-                <Select v-model="selectedDeck" :options="type" placeholder="Select a Deck Type" class="w-full md:w-56" />
+            <div class="w-96 gap-4" style="margin-left: 12px">
+                <Select v-model="selectedDeck" :options="type" optionLabel="name" placeholder="Select a Deck Type" />
             </div>
-            <div class="w-64 gap-4 ring ring-cyan-50 hover:ring-cyan-800" style="margin-left: 12px">
-                <label for="slope">Slope</label><label class="px-1" style="color: red">*</label>
+            <div class="w-64 flex flex-col gap-2 mt-3 mb-3 ring ring-cyan-50 hover:ring-cyan-800" style="margin-left: 12px">
+                <label for="slope" style="color: red">Slope *</label>
 
                 <InputText id="slope" v-model="slope" type="text" placeholder="slope" :invalid="slope === null" @change="valueEntered" />
             </div>
 
-            <div class="w-64 ring ring-cyan-50 hover:ring-cyan-800" style="margin-left: 12px">
-                <label for="height">Height</label><label class="px-1" style="color: red">*</label>
+            <div class="w-64 flex flex-col flex-row gap-2 mt-3 mb-3 ring ring-cyan-50 hover:ring-cyan-800" style="margin-left: 12px">
+                <label for="height" style="color: red">Height *</label>
                 <InputText id="height" v-model="dims.height" type="text" placeholder="height" />
             </div>
-            <div class="w-64 ring ring-cyan-50 hover:ring-cyan-800" style="margin-left: 12px">
+            <div class="w-64 flex flex-col gap-2 mt-3 mb-3 ring ring-cyan-50 hover:ring-cyan-800" style="margin-left: 20px">
                 <label for="area">Area</label>
                 <InputText id="area" v-model="dims.area" type="text" placeholder="area" />
             </div>
-            <div class="w-64 ring ring-cyan-50 hover:ring-cyan-800" style="margin-left: 12px">
-                <label for="height"> Roof Perimeter (a') = .6 x h:</label><label class="px-1" style="color: red">*</label>
+            <div class="w-64 flex flex-col gap-2 mt-3 mb-3 ring ring-cyan-50 hover:ring-cyan-800" style="margin-left: 12px">
+                <label for="height" style="color: red"> Roof Perimeter * (a') = .6 x h:</label>
                 <InputText id="height" v-model="dims.perimeter" type="text" placeholder="perimeter" />
             </div>
             <DripEdgeComponent />
