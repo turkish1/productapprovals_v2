bf830b3548aad336509830e7be9f755a3331c977
janus1947 <devuser1947@gmail.com>
2024-08-30 08:17:02 -0400

    secrets removed

--- src/components/DropZone/s3.js
+++ src/components/DropZone/s3.js
@@ -1,20 +1,2 @@
-const config = {
-    bucketName: 'myBucket',
-    dirName: 'photos' /* optional */,
-    region: 'eu-west-1',
-    accessKeyId: '',
-    secretAccessKey: '',
-    s3Url: 'https://my-s3-url.com/' /* optional */
-};
 
-// const region = 'us-east-1';
-// const bucketName = 'digitalsolutionsroofs';
-// const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
-// const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 
-// const s3 = new aws.S3({
-//     region,
-//     accessKeyId,
-//     secretAccessKey,
-//     signatureVersion: '4'
-// });
