859a8a83ec698087f56c5be6d84bf1b4c9603fb3
Hugo <hugo@Hugos-MBP.attlocal.net>
2024-09-04 07:09:00 -0400

    changes

--- src/components/DropZone/uploadPost.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-import { useAxios } from '@vueuse/integrations/useAxios';
-
-const instance = axios.create({
-    baseURL: '/api'
-});
-
-const { data, isFinished } = useAxios('/upload', { method: 'POST' }, instance);
