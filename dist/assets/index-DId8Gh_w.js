a00f7da33651a7f2180fbfab5eebc1fc9c54c3a2
janus1947 <devuser1947@gmail.com>
2025-01-13 15:30:17 -0500

    added checkTile function o onInput method

--- dist/assets/index-DId8Gh_w.js
+++
@@ -1,1 +0,0 @@
-import{ba as a}from"./index-Cs6-1ItK.js";var e=a();export{e as O};
---
+++ dist/assets/tilenoaStore-Duc0_zeu.js
@@ -0,0 +1,1 @@
+import{ay as s,r,a9 as u}from"./index-BFqjGFLr.js";const n=s(()=>{const a=r([]),t=u(()=>a.value);function e(o){a.value.push(o)}return{tilenoa:a,getNumber:t,addNoa:e}});export{n as u};
