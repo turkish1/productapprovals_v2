4534fd59c55d7672b1cd634b8b6318bd783fd004
janus1947 <devuser1947@gmail.com>
2025-05-14 07:47:42 -0400

    worked on shinglenoa

--- src/components/roofSystems/AutoComplete.vue
+++ src/components/roofSystems/AutoComplete.vue
@@ -65,17 +65,14 @@ const filteredSuggestions = computed(() => {
 
     shingleData.value = suggestions.value[0]?.shingleNoaNumber?.noa;
     console.log('line 67', shingleData.value);
-    shingleIterate.value = shingleData.value;
+    shingleIterate.value = shingleData.value.body ?? [];
     console.log('line 69', shingleIterate.value);
     const stringyfied1 = JSON.stringify(shingleIterate.value).split('[').join();
-
     const stringyfied2 = JSON.stringify(stringyfied1).split(']').join();
     const newArray = computed(() => stringyfied2.split(',').map((s) => s.trim()));
     console.log(newArray.value);
 
     return newArray.value.filter((item) => item.toString().includes(query.value));
-
-    // return suggestions.value[0].shingleNoaNumber.noa.filter((item) => item.toString().includes(query.value));
 });
 
 let datamounted = ref(inputshingle._object.inputshingle);
