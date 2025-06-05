2ea13116406012deadcfbb0f4a404d48c510de26
janus1947 <devuser1947@gmail.com>
2025-06-05 16:41:51 -0400

    composable

---
+++ dist/assets/processStore-X5TtYzcT.js
@@ -0,0 +1,1 @@
+import{M as t}from"./index-D1pNAj1J.js";const o=t("processinput",{state:()=>({processinput:[],id:0}),actions:{addData(s){this.processinput.push({procData:s,completed:!1})}}});export{o as u};
