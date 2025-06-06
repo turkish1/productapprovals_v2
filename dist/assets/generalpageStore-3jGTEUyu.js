57e238dfad169982c2ac30ec0eb5c2dca5f93c47
janus1947 <devuser1947@gmail.com>
2025-06-06 11:59:33 -0400

    paymentgateway

---
+++ dist/assets/generalpageStore-3jGTEUyu.js
@@ -0,0 +1,1 @@
+import{M as a}from"./index-G5mPgPjO.js";const p=a("generalpdfinput",{state:()=>({generalpdfinput:[],id:0}),actions:{addgeneralpdfData(e){this.generalpdfinput.push({generalpdfData:e,completed:!1})}},getters:{generalpdfData:e=>e.generalpdfData}});export{p as u};
