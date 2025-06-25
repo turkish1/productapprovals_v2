6f8d6f1fa588b2e3e1bac479b6701b4e5945786d
janus1947 <devuser1947@gmail.com>
2025-06-25 16:57:49 -0400

    area fix for shingle and lowslope

---
+++ dist/assets/countStore-B6SYNvO9.js
@@ -0,0 +1,1 @@
+import{z as o}from"./index-CyJ3E9hZ.js";const n=o("countinput",{state:()=>({countinput:[],id:0}),actions:{addCount(t){this.countinput.push({countData:t,completed:!1})},reset(){this.countinput=[]},persist:!0}});export{n as c};
