6c4444671e4f2887c53d824fa974e551ad0f35ff
janus1947 <devuser1947@gmail.com>
2025-07-20 18:04:12 -0400

    modified summary,logicbur2,burpdf

--- dist/assets/generalpageStore-BowC_osP.js
+++
@@ -1,1 +0,0 @@
-import{G as a}from"./index-CjgR5uoQ.js";const p=a("generalpdfinput",{state:()=>({generalpdfinput:[],id:0}),actions:{addgeneralpdfData(e){this.generalpdfinput.push({generalpdfData:e,completed:!1})}},getters:{generalpdfData:e=>e.generalpdfData}});export{p as u};
