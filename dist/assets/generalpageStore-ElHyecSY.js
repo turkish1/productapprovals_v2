eb2e9cfe9e338844a9aa1affc8906e8d2a26ec5a
janus1947 <devuser1947@gmail.com>
2025-07-21 08:45:52 -0400

    modified genral, tileMech pdf

---
+++ dist/assets/generalpageStore-ElHyecSY.js
@@ -0,0 +1,1 @@
+import{G as a}from"./index-mr3cNL4_.js";const p=a("generalpdfinput",{state:()=>({generalpdfinput:[],id:0}),actions:{addgeneralpdfData(e){this.generalpdfinput.push({generalpdfData:e,completed:!1})}},getters:{generalpdfData:e=>e.generalpdfData}});export{p as u};
--- dist/assets/processStore-BcMW7kd7.js
+++
@@ -1,1 +0,0 @@
-import{G as t}from"./index-DHSeBDTP.js";const o=t("processinput",{state:()=>({processinput:[],id:0}),actions:{addData(s){this.processinput.push({procData:s,completed:!1})}}});export{o as u};
