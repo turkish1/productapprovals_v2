03229cbf3df7d3e32ecb88f957a8bfaef2c9b120
janus1947 <devuser1947@gmail.com>
2025-07-21 21:50:02 -0400

    tileMechanical

--- dist/assets/generalpageStore-DI_amFFe.js
+++
@@ -1,1 +0,0 @@
-import{E as a}from"./index-BJpXL_9g.js";const p=a("generalpdfinput",{state:()=>({generalpdfinput:[],id:0}),actions:{addgeneralpdfData(e){this.generalpdfinput.push({generalpdfData:e,completed:!1})}},getters:{generalpdfData:e=>e.generalpdfData}});export{p as u};
