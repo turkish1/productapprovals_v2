093abba29f34144fdfbf03b97fa6e53b4f2d55ea
janus1947 <devuser1947@gmail.com>
2025-08-01 10:43:21 -0400

    cloud pdf

---
+++ dist/assets/shingleStore-CHWB7i7u.js
@@ -0,0 +1,1 @@
+import{s as t}from"./reset-store-CIMedAH0.js";import{E as o,aq as i}from"./index-B4ipf8sK.js";const l=o("inputshingle",{state:()=>({inputshingle:[],id:0}),actions:{addShingle(e){console.log(e),this.inputshingle.push({shingleData:e,completed:!1})}},getters:{shingleData:e=>e.shingleData}}),s=i();s.use(t);console.log(s);export{l as u};
