1d0d55d7ec11c3b398444653fd1f8cc6e821a406
janus1947 <devuser1947@gmail.com>
2025-08-03 09:16:57 -0400

    cloud pdfv6

---
+++ dist/assets/shingleStore-CHVwEPfU.js
@@ -0,0 +1,1 @@
+import{s as t}from"./reset-store-W9v74py_.js";import{E as o,aq as i}from"./index-PVYDfCwY.js";const l=o("inputshingle",{state:()=>({inputshingle:[],id:0}),actions:{addShingle(e){console.log(e),this.inputshingle.push({shingleData:e,completed:!1})}},getters:{shingleData:e=>e.shingleData}}),s=i();s.use(t);console.log(s);export{l as u};
