2793d0a498f947406b470e9fae77c58f9e82bbbc
janus1947 <devuser1947@gmail.com>
2025-08-18 12:14:25 -0400

    code changes for modalsv3

---
+++ dist/assets/burpdfStore-CyGssDFy.js
@@ -0,0 +1,1 @@
+import{E as p}from"./index-BctPUBY6.js";const d=p("burpdfinput",{state:()=>({burpdfinput:[],id:0}),actions:{addpdfData(t){this.burpdfinput.push({burpdfData:t,completed:!1})}},getters:{burpdfData:t=>t.burpdfData}});export{d as u};
---
+++ dist/assets/shingleStore-BbigOpW8.js
@@ -0,0 +1,1 @@
+import{s as t}from"./reset-store-BGwD_3MB.js";import{E as o,ap as i}from"./index-BctPUBY6.js";const l=o("inputshingle",{state:()=>({inputshingle:[],id:0}),actions:{addShingle(e){console.log(e),this.inputshingle.push({shingleData:e,completed:!1})}},getters:{shingleData:e=>e.shingleData}}),s=i();s.use(t);console.log(s);export{l as u};
