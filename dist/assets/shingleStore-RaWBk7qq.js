8e877c92fe3b60e89c3318e0758b3b2017a989a7
janus1947 <devuser1947@gmail.com>
2025-08-25 12:02:33 -0400

    code change shingle

--- dist/assets/shingleStore-RaWBk7qq.js
+++
@@ -1,1 +0,0 @@
-import{s as t}from"./reset-store-GeI_p6Ie.js";import{E as o,ap as i}from"./index-DeFQHjIT.js";const l=o("inputshingle",{state:()=>({inputshingle:[],id:0}),actions:{addShingle(e){console.log(e),this.inputshingle.push({shingleData:e,completed:!1})}},getters:{shingleData:e=>e.shingleData}}),s=i();s.use(t);console.log(s);export{l as u};
