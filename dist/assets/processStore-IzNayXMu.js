e5c8402f13121f4f8b72a18d2bc4be6bca3a1b23
janus1947 <devuser1947@gmail.com>
2025-08-30 09:35:21 -0400

    code change tile and mechanical

---
+++ dist/assets/processStore-IzNayXMu.js
@@ -0,0 +1,1 @@
+import{E as t}from"./index-CcPSYwZu.js";const o=t("processinput",{state:()=>({processinput:[],id:0}),actions:{addData(s){this.processinput.push({procData:s,completed:!1})}}});export{o as u};
