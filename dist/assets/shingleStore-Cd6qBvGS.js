c4a9d53bd113275c645a6baea6a62ade598d588c
janus1947 <devuser1947@gmail.com>
2025-09-01 06:07:40 -0400

    shingle, terminal roofsystems

---
+++ dist/assets/shingleStore-Cd6qBvGS.js
@@ -0,0 +1,1 @@
+import{s as t}from"./reset-store-rz9YMJCm.js";import{E as o,ap as i}from"./index-TJizT0Z-.js";const l=o("inputshingle",{state:()=>({inputshingle:[],id:0}),actions:{addShingle(e){console.log(e),this.inputshingle.push({shingleData:e,completed:!1})}},getters:{shingleData:e=>e.shingleData}}),s=i();s.use(t);console.log(s);export{l as u};
