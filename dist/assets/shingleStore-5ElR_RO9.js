508c369dee95df556caf6f60ee208d2a93056a80
janus1947 <devuser1947@gmail.com>
2025-09-14 12:42:15 -0400

    mechtile, shingle, someTile

---
+++ dist/assets/shingleStore-5ElR_RO9.js
@@ -0,0 +1,1 @@
+import{s as t}from"./reset-store-B9zVXPJG.js";import{E as o,ap as i}from"./index-25ndKMvN.js";const l=o("inputshingle",{state:()=>({inputshingle:[],id:0}),actions:{addShingle(e){console.log(e),this.inputshingle.push({shingleData:e,completed:!1})}},getters:{shingleData:e=>e.shingleData}}),s=i();s.use(t);console.log(s);export{l as u};
