fe68640a70209bf6eee85aebb84cc5372a074497
janus1947 <devuser1947@gmail.com>
2025-11-17 10:56:23 -0500

    fix active

---
+++ dist/assets/shingleStore-1i18_XPv.js
@@ -0,0 +1,1 @@
+import{s as t}from"./reset-store-gNK3Z7PN.js";import{D as o,aq as i}from"./index-DHRPd5uZ.js";const l=o("inputshingle",{state:()=>({inputshingle:[],id:0}),actions:{addShingle(e){console.log(e),this.inputshingle.push({shingleData:e,completed:!1})}},getters:{shingleData:e=>e.shingleData}}),s=i();s.use(t);console.log(s);export{l as u};
