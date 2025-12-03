0e2369b971069fec0b6641f11c43cffdad1430f6
janus1947 <devuser1947@gmail.com>
2025-12-03 08:24:00 -0500

    shingle dripedge

--- dist/assets/shingleStore-BUFmZapj.js
+++
@@ -1,1 +0,0 @@
-import{s as t}from"./reset-store-BZI5B_Y-.js";import{D as o,ar as i}from"./index-CdUJRzki.js";const l=o("inputshingle",{state:()=>({inputshingle:[],id:0}),actions:{addShingle(e){console.log(e),this.inputshingle.push({shingleData:e,completed:!1})}},getters:{shingleData:e=>e.shingleData}}),s=i();s.use(t);console.log(s);export{l as u};
---
+++ dist/assets/tilevalueStore-BJo_hW0q.js
@@ -0,0 +1,1 @@
+import{D as t}from"./index-BYZ3DIEY.js";const l=t("tileInputvalues",{state:()=>({tileInputvalues:[]}),actions:{addSystemvalues(e){this.tileInputvalues.push({tileValues:e,completed:!1})},reset(){this.tileInputvalues=[]}},getters:{getTilevalue:e=>e.tileInputvalues}});export{l as u};
