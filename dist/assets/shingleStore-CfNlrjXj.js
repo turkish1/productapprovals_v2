35473f2eb00b8fcb2ce9bb1206327ea96bd5890f
janus1947 <devuser1947@gmail.com>
2025-12-08 00:52:41 -0500

    mechanical changes, shingle submit button version 2

---
+++ dist/assets/shingleStore-CfNlrjXj.js
@@ -0,0 +1,1 @@
+import{s as t}from"./reset-store-CMzO0nft.js";import{D as o,ar as i}from"./index-BuvvX_vs.js";const l=o("inputshingle",{state:()=>({inputshingle:[],id:0}),actions:{addShingle(e){console.log(e),this.inputshingle.push({shingleData:e,completed:!1})}},getters:{shingleData:e=>e.shingleData}}),s=i();s.use(t);console.log(s);export{l as u};
