f26b48fbb0494203e7632ef3fcd708c5718e8ab9
janus1947 <devuser1947@gmail.com>
2026-01-06 13:21:28 -0500

    license stat fix

--- dist/assets/processStore-Czrk4Eu1.js
+++
@@ -1,1 +0,0 @@
-import{D as t}from"./index-Bykl6FuU.js";const o=t("processinput",{state:()=>({processinput:[],id:0}),actions:{addData(s){this.processinput.push({procData:s,completed:!1})}}});export{o as u};
