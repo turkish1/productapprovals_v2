20455da7062fa83b093de597091aa0305087d4d7
janus1947 <devuser1947@gmail.com>
2026-02-02 23:38:02 -0500

    changes to roofsystems to capture muninumber

--- dist/assets/shingleStore-BW5abIG_.js
+++
@@ -1,1 +0,0 @@
-import{s as t}from"./reset-store-CyI6FCRb.js";import{D as o,ar as i}from"./index-DKVSdg1j.js";const l=o("inputshingle",{state:()=>({inputshingle:[],id:0}),actions:{addShingle(e){console.log(e),this.inputshingle.push({shingleData:e,completed:!1})}},getters:{shingleData:e=>e.shingleData}}),s=i();s.use(t);console.log(s);export{l as u};
