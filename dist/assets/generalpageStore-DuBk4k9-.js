85189f3706bd661192bf560f4706fed9ef8aa0eb
janus1947 <devuser1947@gmail.com>
2026-02-03 09:38:52 -0500

    changes to roofsystems to capture muninumber

--- dist/assets/generalpageStore-DuBk4k9-.js
+++
@@ -1,1 +0,0 @@
-import{D as a}from"./index-Xp8LqGSK.js";const p=a("generalpdfinput",{state:()=>({generalpdfinput:[],id:0}),actions:{addgeneralpdfData(e){this.generalpdfinput.push({generalpdfData:e,completed:!1})}},getters:{generalpdfData:e=>e.generalpdfData}});export{p as u};
